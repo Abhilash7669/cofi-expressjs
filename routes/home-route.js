d2ba3af27df455e8d7001e36e91680277f678cef
Abhilash7669 <abhilashsk1998@gmail.com>
2025-03-25 20:59:11 +0530

    added redis check on fetch data

--- routes/home-route.js
+++ routes/home-route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { heroCMS } from "../controller/home-cms.js";
+import { redis } from "../middleware/redis/redisCheck.js";
 
 const route = express.Router();
 const PATH = "/";
 
-route.get(PATH, heroCMS.getHeroContent);
+route.get(PATH, redis.dataCheck("hero"), heroCMS.getHeroContent);
 
 export { route as homeRoute };
