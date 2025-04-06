92ee560963b8198fe7a32af17cf8fd9265915090
Abhilash7669 <abhilashsk1998@gmail.com>
2025-04-06 21:36:08 +0530

    testing express-validator

--- routes/home-route.js
+++ routes/home-route.js
@@ -1,10 +1,24 @@
 import express from "express";
 import { redis } from "../middleware/redis/redisCheck.js";
 import { commonCMSController } from "../controller/cms/common-cms.js";
+import { matchedData, query, validationResult } from "express-validator";
+
 
 const route = express.Router();
 const PATH = "/";
 
 route.get(PATH, redis.dataCheck("homepage"), commonCMSController.fetchContent);
+
+route.get(`${PATH}hello`, query("person").notEmpty().escape(), (req, res) => {
+
+    const result = validationResult(req);
+
+    if (result.isEmpty()) {
+        const data = matchedData(req);
+        return res.send(`Hello, ${data.person}`);
+    }
+
+    res.send({ error: result.array() });
+});
 
 export { route as homeRoute };
